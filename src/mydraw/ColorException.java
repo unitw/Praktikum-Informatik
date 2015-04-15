53fd898f084689d9a3d48781f25d0c65814057a0
3flim <3flim@rzpc162.informatik.uni-hamburg.de>
2015-04-15 16:13:35 +0200

    1.3 Api-Methoden implementiert Foreground and Background fehler

---
+++ src/mydraw/ColorException.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mydraw;
+
+/**
+ *
+ * @author 3flim
+ */
+public class ColorException extends Exception{
+    
+    public ColorException() {}
+
+      //Constructor that accepts a message
+      public ColorException(String message)
+      {
+         super(message);
+      }
+    
+    
+    
+    
+}
---
+++ src/mydraw/Zeichenpanel.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mydraw;
+
+import java.awt.Color;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author 3flim
+ */
+public class Zeichenpanel extends JPanel {
+    public Color color;
+    
+    
+//     public void setWidth(int width){
+//        this.setSize(width, this.getHeight());
+//    }
+//    public void setHeight(int Height){
+//        this.setSize(this.getWidth(), Height);
+//    }
+//    
+//    
+//    
+//      public int getWidth(){
+//      return  this.getWidth();
+//    }
+//    
+//      
+//      public int getHeight(){
+//        return this.getHeight();
+//    }
+//    
+     //asdf
+    
+    
+}
